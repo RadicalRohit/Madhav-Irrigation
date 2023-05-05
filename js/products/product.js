ef8327584ae773ffc228a7bf126a9f6c6de5e8f7
Rohit Jadhav <rohit.ab.jadhav@gmail.com>
2023-05-05 10:44:27 +0530

    category removed and will be fixed in future update

--- js/products/product.js
+++ js/products/product.js
@@ -1,42 +1,18 @@
 // import products from './products.json' assert { type: 'JSON' };
 // console.log(products);
 
-
-
 const response = await fetch("./js/products/products.json");
 const products = await response.json();
 
-console.log(products[1].category);
-
-
 // loop all categories from products.json only unique
-const categories = products.map((product) => product.category);
-const all_categories = [...new Set(categories)];
-// asending order
-all_categories.sort();
 
-
-
-// show all categories on id products_categories
-const products_categories = document.getElementById("products_categories");
-all_categories.forEach((category) => {
-    products_categories.innerHTML += `
-    <div class="category">
-        
-        <button class="select-category">
-            ${category}
-        </button>
-    </div>
-        `;
-
-    }); 
-
-
-
-// show all products on id products
 const products_container = document.getElementById("product-list");
-products.forEach((product) => {
-    products_container.innerHTML += `
+const categories = products.map((product) => product.category);
+const all_categories = [...new Set(categories)].sort();
+
+
+function product_template(product){
+    return `
     <div class="product">
         <div class="product-image">
             <img src="./img/products/${product.category} ${product.name}.png" alt="${product.name}">
@@ -45,16 +21,47 @@ products.forEach((product) => {
             <h6>${product.category}</h6>
             <h4>${product.name}</h4>
             <p><small>${product.description}</small></p>
-            
-            
         </div>
     </div>
         `;
+}
 
-    }   );
 
 
-    // products.forEach((product) => {
-    //     console.log(product.category+" "+product.name+ ".png");
-    // });
+
+// show all categories on id products_categories
+const products_categories = document.getElementById("products_categories");
+// all_categories.forEach((category) => {
+//     products_categories.innerHTML += `
+//     <div class="category">
+//         <button class="select-category" onclick="selectCategory('${category}')" >
+//             ${category}
+//         </button>
+//     </div>
+//         `;
+//     }); 
+
+
+//     function selectCategory(category) {
+//         console.log(category);
+//       }
+
+      
+// document.getElementsByClassName("select-category").onclick = function(){
+//     console.log("clicked");
+// } 
+
+
+
+
+
+
+
+// show all products on id products
+products.forEach((product) => {
+    products_container.innerHTML += product_template(product);
+    }   
+);
+
+
 
